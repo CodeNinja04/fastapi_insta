c151f7103cfb9d3222a91b6014a25b206b6107a8
CodeNinja04 <hemendrasharma2019@gmail.com>
2022-01-02 15:17:35 +0530

    omment section added

--- routers/post.py
+++ routers/post.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Depends,HTTPException, status
+from auth.oauth2 import get_current_user
+from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File
 from sqlalchemy.orm import Session
 from db.database import get_db
 from routers.schemas import PostBase, PostDisplay, UserDisplay
 from db import db_post
 from typing import List
+import random
+import string
+import shutil
+from routers.schemas import UserAuth
 
 router = APIRouter(
     prefix='/post',
@@ -21,4 +26,22 @@ def create(request:PostBase,db:Session=Depends(get_db) ):
 @router.get('',response_model=List[PostDisplay])
 def posts(db:Session=Depends(get_db)):
     return db_post.get_all(db)
-    
+
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
+  letters = string.ascii_letters
+  rand_str = ''.join(random.choice(letters) for i in range(6))
+  new = f'_{rand_str}.'
+  filename = new.join(image.filename.rsplit('.', 1))
+  path = f'images/{filename}'
+
+  with open(path, "w+b") as buffer:
+    shutil.copyfileobj(image.file, buffer)
+
+  return {'filename': path}
+
+
+@router.get('/delete/{id}')
+def delete(id: int, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+  return db_post.delete(db, id, current_user.id)
